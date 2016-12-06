d0fa2025ff2b323381faf965f97ddf4db72283df
Justin Licata <licata.justin@gmail.com>
2016-12-06 09:55:11 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Ayu (dark) for HyperTerm
+
+Ayu for [HyperTerm](https://hyper.is/) is a simple theme with
+bright colors that come from the original
+[Ayu theme for Sublime Text](https://github.com/dempfi/ayu).
+
+A big thank you to that author, [dempfi](https://github.com/dempfi).
+
+---
+
+### Installation
+
+1. Open HyperTerm's preferences with `Cmd + ,` (or manually at `~/.hyperterm.js`).
+2. Update your list of plugins to include `hyperterm-ayu`, like this:
+
+  ```javascript
+plugins: [
+  'hyperterm-ayu'
+]
+```
+3. Fully reload HyperTerm (`Cmd + Shift + R`)
+4. Profit!
+
+### Related
+
+- [HyperTerm](https://hyper.is/)
+- [Ayu for Sublime Text](https://github.com/dempfi/ayu)
+- [Ayu for Vim](https://github.com/ayu-theme/ayu-vim)
+
+### License
+
+MIT @ [Justin Licata](https://twitter.com/justinlicata)
---
+++ index.js
@@ -0,0 +1,69 @@
+// Constants
+const backgroundColor = '#0F1419';
+const foregroundColor = '#e6e1cf';
+
+// Colors
+const RED = '#ff3333';
+const GREEN = '#89b73d';
+const YELLOW = '#f49c20';
+const BLUE = '#3ea7dc';
+const MAGENTA = '#b37ed0';
+const LIGHT_GRAY = '#f2f2f2';
+const DARK_GRAY = '#828c9a'
+
+// Mapped Colors
+const colors = {
+  black: backgroundColor,
+  red: RED,
+  green: GREEN,
+  yellow: YELLOW,
+  blue: BLUE,
+  magenta: MAGENTA,
+  cyan: BLUE,
+  white: LIGHT_GRAY,
+  lightBlack: DARK_GRAY,
+  lightRed: RED,
+  lightGreen: GREEN,
+  lightYellow: YELLOW,
+  lightBlue: BLUE,
+  lightMagenta: MAGENTA,
+  lightCyan: BLUE,
+  colorCubes: '#fff',
+  grayscale: foregroundColor
+};
+
+// Additional Constants
+const cursorColor = YELLOW;
+const borderColor = backgroundColor;
+
+exports.decorateConfig = (config) => {
+  return Object.assign({}, config, {
+    foregroundColor,
+    backgroundColor,
+    borderColor,
+    cursorColor,
+    colors,
+    css: `
+      ${config.css || ''}
+      .cursor-node {
+        background-color: ${MAGENTA} !important;
+        border-color: ${MAGENTA} !important;
+      }
+      .hyper_main {
+        border: none !important;
+      }
+      .header_header {
+        background: ${backgroundColor} !important;
+      }
+      .splitpane_divider {
+        background-color: rgba(130, 128, 184, 0.5) !important;
+      }
+      .tab_tab {
+        border: 0;
+      }
+      .tab_textActive {
+        border-bottom: 2px solid ${YELLOW};
+      }
+    `
+  })
+}
